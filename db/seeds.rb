1dbfddbe82608efe94979e9cdea9ce97b3437c02
Edward Lee <eddy2706@gmail.com>
2018-11-12 17:39:01 +0900

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+
+puts 'Creating 100 fake trash...'
+100.times do
+  user = User.new(
+    is_scout: [true,false].sample,
+    email: Faker::Internet.email,
+    first_name: Faker::GameOfThrones.character,
+    last_name: Faker::GameOfThrones.character,
+    password: Faker::Internet.password,
+    location: Faker::Address.country
+  )
+  p user
+  user.save!
+end
+
+  scout = User.all.select do |user|
+    user.is_scout == true
+    end
+
+  scout.each do |scout|
+    rand(1..5).times do
+      location = Location.new(
+        name: Faker::FunnyName.name,
+        description: Faker::DragonBall.character,
+        coordinates: Faker::Address.full_address,
+        price: rand(1..50)
+        )
+      location.user_id = scout.id
+      location.save
+      p location
+    end
+  end
+
+puts 'Finished!'
