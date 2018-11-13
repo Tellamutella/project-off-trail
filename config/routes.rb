709cd637f86da628390910665ebe9720bac9e7bd
Edward Lee <eddy2706@gmail.com>
2018-11-13 13:38:34 +0900

    fixed index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookings/create'
-  get 'bookings/destroy'
-  get 'locations/new'
-  get 'locations/create'
-  get 'locations/edit'
-  get 'locations/update'
-  get 'locations/destroy'
-  get 'locations/index'
-  get 'locations/show'
   devise_for :users
   root to: 'pages#home'
   get 'dashboard', to: 'users#dashboard'
