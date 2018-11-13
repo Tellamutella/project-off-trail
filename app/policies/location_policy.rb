372bb7e1691da7b4fb0d80e1399256bd27b933e5
nlogx <lina.xiao@live.com>
2018-11-13 13:42:13 +0900

    saving index link to fixed

--- app/policies/location_policy.rb
+++ app/policies/location_policy.rb
@@ -1,7 +1,7 @@
 class LocationPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.where(user: @user) # Location.where(user is current user)
+      scope.all # Location.where(user is current user)
     end
   end
 
